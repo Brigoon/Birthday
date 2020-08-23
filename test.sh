85c6fff9e3ccc19f1cfbba9dc3be7323ddc8e3fa
Brigoon <btomiuk1@gmail.com>
2020-08-23 12:59:42 -0400

    test file

---
+++ test.sh
@@ -0,0 +1,12 @@
+time ./Birthday.exe 09/11/2020
+time ./Birthday.exe 08/12/2020
+time ./Birthday.exe 01/01/2000
+time ./Birthday.exe 03/01/2000
+time ./Birthday.exe 01/01/2004
+time ./Birthday.exe 03/01/2004
+time ./Birthday.exe 01/01/2100
+time ./Birthday.exe 03/01/2100
+time ./Birthday.exe 01/01/2101
+time ./Birthday.exe 01/01/2400
+time ./Birthday.exe 03/01/2400
+time ./Birthday.exe 01/01/2401
